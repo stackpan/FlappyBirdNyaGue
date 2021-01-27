759c4be6440c759bfbfedbed70c227d64d08097f
vanefka <ivanrizqy.ir@gmail.com>
2021-01-27 09:15:58 +0700

    Menambahkan Game State

---
+++ desktop/src/com/ivanzkyanto/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.ivanzkyanto.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ivanzkyanto.flappybird.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = MyGame.WIDTH;
+		config.height = MyGame.HEIGHT;
+		config.title = MyGame.TITLE;
+		new LwjglApplication(new MyGame(), config);
+	}
+}
