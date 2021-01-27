c43d3e4bfdf39122997da27e9448c49474d854c8
vanefka <ivanrizqy.ir@gmail.com>
2021-01-27 21:04:24 +0700

    Menambahkan rintangan (obstacle)

--- core/src/com/ivanzkyanto/flappybird/sprites/Tube.java
+++ core/src/com/ivanzkyanto/flappybird/sprites/Tube.java
@@ -1,2 +1,45 @@
-package com.ivanzkyanto.flappybird.sprites;public class Tube {
+package com.ivanzkyanto.flappybird.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+public class Tube {
+    private static final int FLUCTUATION = 150; // Jarak yang bisa dirandom
+    private static final int TUBE_GAP = 80; // Jarak celah antara Tube atas dgn Tube bawah
+    private static final int LOWEST_OPENING = 120;
+    private Texture topTube, botTube;
+    private Vector2 posTopTube, posBotTube;
+    private Random rand;
+
+    public Tube(float x) {
+        topTube = new Texture("toptube.png");
+        botTube = new Texture("bottube.png");
+        rand = new Random();
+
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + TUBE_GAP + LOWEST_OPENING);
+        posBotTube = new Vector2(x, posTopTube.y - TUBE_GAP - botTube.getHeight());
+    }
+
+
+    public static int getFLUCTUATION() {
+        return FLUCTUATION;
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBotTube() {
+        return botTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBotTube() {
+        return posBotTube;
+    }
 }
--- core/src/com/ivanzkyanto/flappybird/states/PlayState.java
+++ core/src/com/ivanzkyanto/flappybird/states/PlayState.java
@@ -6,17 +6,20 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ivanzkyanto.flappybird.MyGame;
 import com.ivanzkyanto.flappybird.sprites.Bird;
+import com.ivanzkyanto.flappybird.sprites.Tube;
 
 public class PlayState extends State {
 
     private Bird bird;
     private Texture bg;
+    private Tube tube;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Bird(50, 200);
+        bird = new Bird(40, 200);
         cam.setToOrtho(false, MyGame.WIDTH / 2, MyGame.HEIGHT / 2);
         bg = new Texture("bg.png");
+        tube = new Tube(160);
     }
 
     @Override
@@ -36,6 +39,8 @@ public class PlayState extends State {
         sb.begin();
         sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosBotTube().y);
+        sb.draw(tube.getBotTube(), tube.getPosBotTube().x, tube.getPosTopTube().y);
         sb.end();
     }
 
